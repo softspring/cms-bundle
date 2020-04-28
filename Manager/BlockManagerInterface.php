5cb8ee2eed41a4903068d973f4e6691123fc6ca9
Javi H. Gil <javihgil@gmail.com>
2020-04-28 11:49:42 +0200

    Update manager interfaces doc blocks

--- Manager/BlockManagerInterface.php
+++ Manager/BlockManagerInterface.php
@@ -2,9 +2,23 @@
 
 namespace Softspring\CmsBundle\Manager;
 
+use Softspring\CmsBundle\Model\BlockInterface;
 use Softspring\CrudlBundle\Manager\CrudlEntityManagerInterface;
 
 interface BlockManagerInterface extends CrudlEntityManagerInterface
 {
+    /**
+     * @return BlockInterface
+     */
+    public function createEntity();
 
+    /**
+     * @param BlockInterface $entity
+     */
+    public function saveEntity($entity): void;
+
+    /**
+     * @param BlockInterface $entity
+     */
+    public function deleteEntity($entity): void;
 }
--- Manager/SiteManagerInterface.php
+++ Manager/SiteManagerInterface.php
@@ -2,9 +2,23 @@
 
 namespace Softspring\CmsBundle\Manager;
 
+use Softspring\CmsBundle\Model\SiteInterface;
 use Softspring\CrudlBundle\Manager\CrudlEntityManagerInterface;
 
 interface SiteManagerInterface extends CrudlEntityManagerInterface
 {
+    /**
+     * @return SiteInterface
+     */
+    public function createEntity();
 
+    /**
+     * @param SiteInterface $entity
+     */
+    public function saveEntity($entity): void;
+
+    /**
+     * @param SiteInterface $entity
+     */
+    public function deleteEntity($entity): void;
 }
