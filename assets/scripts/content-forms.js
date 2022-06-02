2094ff37acdbdf40a7302035c74ddac4e03210e2
ebpe <ebelmonte82@gmail.com>
2022-06-02 14:29:08 +0200

    Fixed admin width

--- assets/scripts/content-forms.js
+++ assets/scripts/content-forms.js
@@ -7,12 +7,19 @@ window.addEventListener('load', (event) => {
     function moduleFocus(module) {
         allLostFocus();
         module.classList.add('active');
+
         document.getElementById('content-form').classList.remove('d-none');
+        // If has form to show
+        if(module.closest('div').querySelector('.active >.cms-module-body > .cms-module-edit > .cms-module-form')) {
+            document.querySelectorAll('.polymorphic-collection-widget').forEach((element) => element.classList.add('has-form'));
+        }
     }
 
     function allLostFocus() {
         document.getElementById('content-form').classList.add('d-none');
         document.querySelectorAll('.cms-module').forEach((element) => element.classList.remove('active'));
+        document.querySelectorAll('.polymorphic-collection-widget').forEach((element) => element.classList.remove('has-form'));
+
     }
 
     // close module edit form
