88fffdebc6ab0fe8411c5f77476760365bb605d5
Javi H. Gil <javihgil@softspring.es>
2022-06-13 18:24:44 +0200

    Add translatable html module

---
+++ src/Form/Type/TranslatableType.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Softspring\CmsBundle\Form\Type;
+
+use Softspring\Component\DynamicFormType\Form\DynamicFormTrait;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TranslatableType extends AbstractType
+{
+    use DynamicFormTrait;
+
+    protected string $defaultLocale;
+    protected array $enabledLocales;
+
+    public function __construct(string $defaultLocale, array $enabledLocales)
+    {
+        $this->defaultLocale = $defaultLocale;
+        $this->enabledLocales = $enabledLocales;
+    }
+
+    public function getBlockPrefix(): string
+    {
+        return 'translatable';
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'required' => false,
+            'languages' => $this->enabledLocales,
+            'default_language' => $this->defaultLocale,
+            'children_attr' => [],
+            'type' => null,
+            'type_options' => [],
+        ]);
+
+        $resolver->setRequired('languages');
+        $resolver->setAllowedTypes('languages', 'array');
+        $resolver->setRequired('default_language');
+        $resolver->setAllowedTypes('default_language', 'string');
+        $resolver->setRequired('type');
+        $resolver->setAllowedTypes('type', 'string');
+        $resolver->setAllowedTypes('type_options', 'array');
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        foreach ($options['languages'] as $lang) {
+            $builder->add($lang, $this->getFieldType($options['type']), [
+                'required' => $lang == $options['default_language'],
+                'label' => $lang,
+                'block_prefix' => 'translatable_element',
+            ] + $options['type_options']
+            + [
+                'attr' => $options['children_attr'] + ($options['type_options']['attr']??[]),
+            ]);
+        }
+    }
+
+    protected function getFormClasses(string $type): array
+    {
+        return [
+            'App\Form\Type\\'.ucfirst($type).'Type',
+            'Softspring\CmsBundle\Form\Type\\'.ucfirst($type).'Type',
+            'Symfony\Component\Form\Extension\Core\Type\\'.ucfirst($type).'Type',
+        ];
+    }
+}
