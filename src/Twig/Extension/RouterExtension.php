573f6eace92f251abde9a9d23261b09be9a192f0
Javi H. Gil <javihgil@softspring.es>
2022-08-04 11:02:41 +0200

    Refactor routing

--- src/Twig/Extension/RouterExtension.php
+++ src/Twig/Extension/RouterExtension.php
@@ -21,10 +21,14 @@ class RouterExtension extends AbstractExtension
     public function getFunctions(): array
     {
         return [
+            // old functions, to be deleted soon
             new TwigFunction('sfs_cms_url', [$this->urlGenerator, 'getUrl'], ['deprecated' => true, 'alternate' => 'url']),
             new TwigFunction('sfs_cms_path', [$this->urlGenerator, 'getPath'], ['deprecated' => true, 'alternate' => 'path']),
-            new TwigFunction('sfs_cms_url_fixed', [$this->urlGenerator, 'getUrlFixed'], ['deprecated' => true, 'alternate' => 'url']),
-            new TwigFunction('sfs_cms_path_fixed', [$this->urlGenerator, 'getPathFixed'], ['deprecated' => true, 'alternate' => 'path']),
+            new TwigFunction('sfs_cms_url_fixed', [$this->urlGenerator, 'getUrlFixed'], ['deprecated' => true, 'alternate' => 'sfs_cms_route_path_url']),
+            new TwigFunction('sfs_cms_path_fixed', [$this->urlGenerator, 'getPathFixed'], ['deprecated' => true, 'alternate' => 'sfs_cms_route_path_path']),
+            // valid functions
+            new TwigFunction('sfs_cms_route_path_url', [$this->urlGenerator, 'getUrlFixed']),
+            new TwigFunction('sfs_cms_route_path_path', [$this->urlGenerator, 'getPathFixed']),
             new TwigFunction('sfs_cms_route_attr', [$this->urlGenerator, 'getRouteAttributes']),
         ];
     }
