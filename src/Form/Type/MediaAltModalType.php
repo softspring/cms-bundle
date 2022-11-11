08298b6c9051eb6e125ee23a11c716fea272da63
Javi H. Gil <javihgil@softspring.es>
2022-11-11 15:09:18 +0100

    Improve media form types

---
+++ src/Form/Type/MediaAltModalType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Softspring\CmsBundle\Form\Type;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class MediaAltModalType extends AbstractType
+{
+    use PropagateLabelFormatTrait;
+
+    public function getBlockPrefix(): string
+    {
+        return 'cms_media_alt_modal';
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'media_type_options' => [],
+            'alt' => true,
+            'alt_type_options' => [],
+        ]);
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('media', MediaModalType::class, $options['media_type_options']);
+
+        if ($options['alt']) {
+            $builder->add('alt', TextType::class, $options['alt_type_options']);
+        }
+    }
+}
--- src/Form/Type/MediaModalType.php
+++ src/Form/Type/MediaModalType.php
@@ -3,41 +3,8 @@
 namespace Softspring\CmsBundle\Form\Type;
 
 use Softspring\MediaBundle\Form\MediaModalType as RealMediaModalType;
-use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\FormBuilderInterface;
-use Symfony\Component\Form\FormInterface;
-use Symfony\Component\Form\FormView;
-use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class MediaModalType extends AbstractType
+class MediaModalType extends RealMediaModalType
 {
-    public function getBlockPrefix(): string
-    {
-        return 'cms_media_modal';
-    }
 
-    public function configureOptions(OptionsResolver $resolver)
-    {
-        $resolver->setDefaults([
-            'media_type_options' => [],
-            'alt' => true,
-            'alt_type_options' => [],
-        ]);
-    }
-
-    public function buildForm(FormBuilderInterface $builder, array $options)
-    {
-        $builder->add('media', RealMediaModalType::class, $options['media_type_options']);
-
-        if ($options['alt']) {
-            $builder->add('alt', TextType::class, $options['alt_type_options']);
-        }
-    }
-
-    public function finishView(FormView $view, FormInterface $form, array $options)
-    {
-        $view->children['media']->vars['label_format'] = str_replace('.form.', ".form.{$view->vars['name']}.", $view->children['media']->vars['label_format']);
-        $view->children['alt']->vars['label_format'] = str_replace('.form.', ".form.{$view->vars['name']}.", $view->children['alt']->vars['label_format']);
-    }
 }
