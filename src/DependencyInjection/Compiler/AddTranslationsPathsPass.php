f4c1a293b2ca4ff9b3d08d647ba0948b398e92c6
Javi H. Gil <javihgil@softspring.es>
2023-01-26 14:53:59 +0100

    Search for blocks translations

--- src/DependencyInjection/Compiler/AddTranslationsPathsPass.php
+++ src/DependencyInjection/Compiler/AddTranslationsPathsPass.php
@@ -11,31 +11,36 @@ class AddTranslationsPathsPass implements CompilerPassInterface
     public function process(ContainerBuilder $container)
     {
         $translatorDefinition = $container->getDefinition('translator.default');
-//        $loaderIds = $translatorDefinition->getArgument(4);
 
         foreach ($container->getParameter('sfs_cms.collections') as $collectionPath) {
             // add modules translations if exists
             $modulesPath = $container->getParameter('kernel.project_dir').'/'.trim($collectionPath, '/').'/modules';
-            if (!is_dir($modulesPath)) {
-                continue;
+            if (is_dir($modulesPath)) {
+                foreach ((new Finder())->directories()->in("$modulesPath")->in('*')->name('translations') as $transDirectory) {
+                    foreach ((new Finder())->in($transDirectory->getRealPath())->files() as $file) {
+                        $fileNameParts = explode('.', $file->getBasename());
+                        $format = array_pop($fileNameParts);
+                        $locale = array_pop($fileNameParts);
+                        $domain = implode('.', $fileNameParts);
+                        $translatorDefinition->addMethodCall('addResource', [$format, $file->getRealPath(), $locale, $domain]);
+                    }
+                }
             }
 
-            foreach ((new Finder())->directories()->in("$modulesPath")->in('*')->name('translations') as $transDirectory) {
-                foreach ((new Finder())->in($transDirectory->getRealPath())->files() as $file) {
-                    $fileNameParts = explode('.', $file->getBasename());
-                    $format = array_pop($fileNameParts);
-                    $locale = array_pop($fileNameParts);
-                    $domain = implode('.', $fileNameParts);
-                    $translatorDefinition->addMethodCall('addResource', [$format, $file->getRealPath(), $locale, $domain]);
 
-//                    $loaderIds['resource_files'][$locale][] = $file->getRealPath();
+            // add blocks translations if exists
+            $blocksPath = $container->getParameter('kernel.project_dir').'/'.trim($collectionPath, '/').'/blocks';
+            if (is_dir($blocksPath)) {
+                foreach ((new Finder())->directories()->in("$blocksPath")->in('*')->name('translations') as $transDirectory) {
+                    foreach ((new Finder())->in($transDirectory->getRealPath())->files() as $file) {
+                        $fileNameParts = explode('.', $file->getBasename());
+                        $format = array_pop($fileNameParts);
+                        $locale = array_pop($fileNameParts);
+                        $domain = implode('.', $fileNameParts);
+                        $translatorDefinition->addMethodCall('addResource', [$format, $file->getRealPath(), $locale, $domain]);
+                    }
                 }
-
-//                $loaderIds['scanned_directories'][] = $transDirectory->getRealPath();
-//                $loaderIds['cache_vary']['scanned_directories'][] = $transDirectory->getRealPath();
             }
         }
-
-//        $translatorDefinition->replaceArgument(4, $loaderIds);
     }
 }
