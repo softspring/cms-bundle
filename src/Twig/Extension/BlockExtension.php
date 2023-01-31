65bbe6af6a91003e58ca5b0a9941255b8ec3e4cf
Javi H. Gil <javihgil@softspring.es>
2023-01-31 12:58:10 +0100

    BUNDLES-134 - add sfs_cms_block_by_id and sfs_cms_block_find twig functions

--- src/Twig/Extension/BlockExtension.php
+++ src/Twig/Extension/BlockExtension.php
@@ -2,16 +2,20 @@
 
 namespace Softspring\CmsBundle\Twig\Extension;
 
+use Softspring\CmsBundle\Manager\BlockManagerInterface;
+use Softspring\CmsBundle\Model\BlockInterface;
 use Softspring\CmsBundle\Render\BlockRenderer;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
 class BlockExtension extends AbstractExtension
 {
+    protected BlockManagerInterface $blockManager;
     protected BlockRenderer $blockRenderer;
 
-    public function __construct(BlockRenderer $blockRenderer)
+    public function __construct(BlockManagerInterface $blockManager, BlockRenderer $blockRenderer)
     {
+        $this->blockManager = $blockManager;
         $this->blockRenderer = $blockRenderer;
     }
 
@@ -20,6 +24,28 @@ class BlockExtension extends AbstractExtension
         return [
             new TwigFunction('sfs_cms_block', [$this->blockRenderer, 'renderBlock'], ['is_safe' => ['html']]),
             new TwigFunction('sfs_cms_block_by_type', [$this->blockRenderer, 'renderBlockByType'], ['is_safe' => ['html']]),
+            new TwigFunction('sfs_cms_block_by_id', [$this, 'renderBlockById'], ['is_safe' => ['html']]),
+            new TwigFunction('sfs_cms_block_find', [$this, 'findOneBy'], ['is_safe' => ['html']]),
         ];
+    }
+
+    public function renderBlockById($id): string
+    {
+        $block = $this->findOneBy($id);
+
+        return $block ? $this->blockRenderer->renderBlock($block) : "<!-- block $id not found -->";
+    }
+
+    public function findOneBy($criteria, array $orderBy = []): ?BlockInterface
+    {
+        if (is_string($criteria)) {
+            $criteria = ['id' => $criteria];
+        }
+
+        if (!is_array($criteria)) {
+            throw new \Exception('Invalid criteria');
+        }
+
+        return $this->blockManager->getRepository()->findOneBy($criteria, $orderBy);
     }
 }
