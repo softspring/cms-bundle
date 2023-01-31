72b19ce75fb578c6fbf91ad3c8217e719914ae77
Javi H. Gil <javihgil@softspring.es>
2023-01-31 12:56:45 +0100

    BUNDLES-134 - refactor content entity listeners transformations to share behaviour with blocks

--- src/EntityListener/ContentVersionListener.php
+++ src/EntityListener/ContentVersionListener.php
@@ -4,14 +4,14 @@ namespace Softspring\CmsBundle\EntityListener;
 
 use Doctrine\ORM\Event\LifecycleEventArgs;
 use Doctrine\ORM\Event\PreUpdateEventArgs;
-use Doctrine\ORM\Mapping\MappingException;
-use Doctrine\Persistence\ObjectManager;
 use Softspring\CmsBundle\Model\ContentVersionInterface;
 use Softspring\CmsBundle\Render\ContentRender;
 use Symfony\Component\HttpFoundation\RequestStack;
 
 class ContentVersionListener
 {
+    use TransformEntityValuesTrait;
+
     protected ContentRender $contentRender;
     protected RequestStack $requestStack;
     protected array $enabledLocales;
@@ -80,12 +80,12 @@ class ContentVersionListener
                 if (isset($module['modules'])) {
                     foreach ($module['modules'] as $sm => $submodule) {
                         foreach ($submodule as $field => $value) {
-                            $data[$layout][$m]['modules'][$sm][$field] = $this->transformExtraDataValue($value, $event->getObjectManager());
+                            $data[$layout][$m]['modules'][$sm][$field] = $this->transformEntityValues($value, $event->getObjectManager());
                         }
                     }
                 } else {
                     foreach ($module as $field => $value) {
-                        $data[$layout][$m][$field] = $this->transformExtraDataValue($value, $event->getObjectManager());
+                        $data[$layout][$m][$field] = $this->transformEntityValues($value, $event->getObjectManager());
                     }
                 }
             }
@@ -105,52 +105,16 @@ class ContentVersionListener
                 if (isset($module['modules'])) {
                     foreach ($module['modules'] as $sm => $submodule) {
                         foreach ($submodule as $field => $value) {
-                            $data[$layout][$m]['modules'][$sm][$field] = $this->untransformExtraDataValue($value, $event->getObjectManager());
+                            $data[$layout][$m]['modules'][$sm][$field] = $this->untransformEntityValues($value, $event->getObjectManager());
                         }
                     }
                 } else {
                     foreach ($module as $field => $value) {
-                        $data[$layout][$m][$field] = $this->untransformExtraDataValue($value, $event->getObjectManager());
+                        $data[$layout][$m][$field] = $this->untransformEntityValues($value, $event->getObjectManager());
                     }
                 }
             }
         }
         $contentVersion->setData($data);
-    }
-
-    protected function transformExtraDataValue($value, ObjectManager $objectManager)
-    {
-        if (is_object($value)) {
-            try {
-                return [
-                    '_entity_class' => get_class($value),
-                    '_entity_id' => $objectManager->getClassMetadata(get_class($value))->getIdentifierValues($value),
-                ];
-            } catch (MappingException $e) {
-            }
-        } elseif (is_array($value)) {
-            foreach ($value as $key => $value2) {
-                $value[$key] = $this->transformExtraDataValue($value2, $objectManager);
-            }
-        }
-
-        return $value;
-    }
-
-    protected function untransformExtraDataValue($value, ObjectManager $objectManager)
-    {
-        if (is_array($value)) {
-            if (isset($value['_entity_class'])) {
-                $repo = $objectManager->getRepository($value['_entity_class']);
-
-                return $repo->findOneBy($value['_entity_id']);
-            } else {
-                foreach ($value as $key => $value2) {
-                    $value[$key] = $this->untransformExtraDataValue($value2, $objectManager);
-                }
-            }
-        }
-
-        return $value;
     }
 }
