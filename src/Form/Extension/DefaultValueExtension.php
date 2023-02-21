30f4f619e8eb8cd80b6e72003a9579a6eba76b04
Javi H. Gil <javihgil@softspring.es>
2023-02-21 18:39:35 +0100

    Allow default values for some form types (required values)

---
+++ src/Form/Extension/DefaultValueExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Softspring\CmsBundle\Form\Extension;
+
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\CallbackTransformer;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class DefaultValueExtension extends AbstractTypeExtension
+{
+    public static function getExtendedTypes(): iterable
+    {
+        return [FormType::class];
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefault('default_value', null);
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        if (empty($options['default_value'])) {
+            return;
+        }
+
+        // TODO this only works on required fields, on other fields it wont allow them to be empty
+        $builder->addModelTransformer(new CallbackTransformer(function ($value) use ($options) {
+            return $value ?? $options['default_value'];
+        }, function ($value) {
+            return $value;
+        }));
+    }
+}
