879673081d9699392f0bb48aff4ffebb2ce99173
Javi H. Gil <javihgil@softspring.es>
2023-03-02 15:05:43 +0100

    Improve invalid module exception message

--- src/Config/Exception/InvalidModuleException.php
+++ src/Config/Exception/InvalidModuleException.php
@@ -6,6 +6,6 @@ class InvalidModuleException extends \Exception
 {
     public function __construct(string $module, array $modulesConfig)
     {
-        parent::__construct(sprintf('Invalid module "%s", posible values: %s', $module, implode(', ', array_keys($modulesConfig))));
+        parent::__construct(sprintf("Invalid module '%s', posible values: %s.\n\nCheck that you have configured all your collections in sfs_cms config.", $module, implode(', ', array_keys($modulesConfig))));
     }
 }
