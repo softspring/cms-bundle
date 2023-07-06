2665b945592f9793dcefd226c02f8016fbf41499
Javi H. Gil <javihgil@softspring.es>
2023-07-06 11:04:51 +0200

    Add JS event listeners tools for content editing

---
+++ assets/scripts/admin/content-edit/event-listeners.js
@@ -0,0 +1,44 @@
+function _addEditEventListener(eventName, listener, moduleName = null, targetMatcher = null) {
+    document.addEventListener(eventName, function (event) {
+        if (!event.target) {
+            return;
+        }
+
+        if (targetMatcher && !event.target.matches(targetMatcher)) {
+            return;
+        }
+
+        let module = event.target.closest('.cms-module');
+        if (!module) {
+            return;
+        }
+
+        let node = module.closest('[data-polymorphic=node]');
+        if (!node) {
+            return;
+        }
+
+        if (moduleName && moduleName !== node.dataset.moduleId) {
+            return;
+        }
+
+        let preview = event.target.closest('.cms-module-edit').querySelector('.module-preview');
+        let form = event.target.closest('.cms-module-edit').querySelector('.cms-module-form');
+
+        return listener(event.target, module, preview, form, event);
+    });
+}
+
+function cmsEditModuleListener(moduleName, targetMatcher, eventName, listener) {
+    _addEditEventListener(eventName, listener, moduleName, targetMatcher);
+}
+
+function cmsEditListener(targetMatcher, eventName, listener) {
+    _addEditEventListener(eventName, listener, null, targetMatcher);
+}
+
+export {
+    cmsEditModuleListener,
+    cmsEditListener,
+};
+
