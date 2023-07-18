cd13301c5ffc493f59d7fad961e5300dc80965de
Javi H. Gil <javihgil@softspring.es>
2023-07-18 23:41:52 +0200

    Add content edition macro helpers

--- assets/scripts/admin-cms.js
+++ assets/scripts/admin-cms.js
@@ -1,3 +1,4 @@
+import './admin/content-edit/config-errors';
 import './admin/content-edit/site-filter-modules';
 import './admin/content-edit/locale-filter-modules';
 import './admin/content-edit/filter-preview';
