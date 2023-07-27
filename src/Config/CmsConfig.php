949d915ba7797f8faac62a0bc0321027071fad4e
Javi H. Gil <javihgil@softspring.es>
2023-07-27 09:52:57 +0200

    Fix sitesTableExists() function in CmsConfig

--- src/Config/CmsConfig.php
+++ src/Config/CmsConfig.php
@@ -153,7 +153,7 @@ class CmsConfig
             $result = $connection->executeQuery('SHOW tables');
             $this->sitesTableExists = false;
             foreach ($result->fetchAllAssociative() as $value) {
-                if (current($value) == 'cms_sites') {
+                if ('cms_sites' == current($value)) {
                     $this->sitesTableExists = true;
                 }
             }
