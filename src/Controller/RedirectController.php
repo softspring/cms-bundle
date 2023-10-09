9417ec1729dc646338c4b87c6f5a5686f3a23d08
Javi H. Gil <javihgil@softspring.es>
2023-10-09 13:06:45 +0200

    Redirect controller uses current locale and site

--- src/Controller/RedirectController.php
+++ src/Controller/RedirectController.php
@@ -4,13 +4,16 @@ namespace Softspring\CmsBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class RedirectController extends AbstractController
 {
-    public function redirection(string $route, array $routeParams = null, int $statusCode = Response::HTTP_PERMANENTLY_REDIRECT): RedirectResponse
+    public function redirection(string $route, array $routeParams = [], int $statusCode = Response::HTTP_PERMANENTLY_REDIRECT): RedirectResponse
     {
-        return $this->redirectToRoute($route, $routeParams ?? [], $statusCode);
+        isset($routeParams['_sfs_cms_locale']) && $routeParams['_locale'] = $routeParams['_sfs_cms_locale'];
+
+        return $this->redirectToRoute($route, $routeParams, $statusCode);
     }
 
     public function redirectToUrl(string $url, int $statusCode = Response::HTTP_PERMANENTLY_REDIRECT): RedirectResponse
