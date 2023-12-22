ef4ad4977b83704b08a68e58c28b8fe471de6cee
Javi H. Gil <javihgil@users.noreply.github.com>
2023-12-22 23:56:57 +0100

    Refactor admin menu controller to crudl listeners (#212)

--- src/SfsCmsEvents.php
+++ src/SfsCmsEvents.php
@@ -4,6 +4,33 @@ namespace Softspring\CmsBundle;
 
 class SfsCmsEvents
 {
+    // MENU LIST EVENTS
+    public const ADMIN_MENUS_LIST_INITIALIZE = 'sfs_cms.admin.menus.list_initialize';
+    public const ADMIN_MENUS_LIST_FILTER = 'sfs_cms.admin.menus.list_filter';
+    public const ADMIN_MENUS_LIST_VIEW = 'sfs_cms.admin.menus.list_view';
+    // MENU CREATE EVENTS
+    public const ADMIN_MENUS_CREATE_INITIALIZE = 'sfs_cms.admin.menus.create_initialize';
+    public const ADMIN_MENUS_CREATE_ENTITY = 'sfs_cms.admin.menus.create_create_entity';
+    public const ADMIN_MENUS_CREATE_FORM_PREPARE = 'sfs_cms.admin.menus.create_form_prepare';
+    public const ADMIN_MENUS_CREATE_FORM_INIT = 'sfs_cms.admin.menus.create_form_init';
+    public const ADMIN_MENUS_CREATE_FORM_VALID = 'sfs_cms.admin.menus.create_form_valid';
+    public const ADMIN_MENUS_CREATE_SUCCESS = 'sfs_cms.admin.menus.create_success';
+    public const ADMIN_MENUS_CREATE_FAILURE = 'sfs_cms.admin.menus.create_failure';
+    public const ADMIN_MENUS_CREATE_FORM_INVALID = 'sfs_cms.admin.menus.create_form_invalid';
+    public const ADMIN_MENUS_CREATE_VIEW = 'sfs_cms.admin.menus.create_view';
+    // MENU UPDATE EVENTS
+    public const ADMIN_MENUS_UPDATE_INITIALIZE = 'sfs_cms.admin.menus.update_initialize';
+    public const ADMIN_MENUS_UPDATE_LOAD_ENTITY = 'sfs_cms.admin.menus.update_load_entity';
+    public const ADMIN_MENUS_UPDATE_NOT_FOUND = 'sfs_cms.admin.menus.update_not_found';
+    public const ADMIN_MENUS_UPDATE_FOUND = 'sfs_cms.admin.menus.update_found';
+    public const ADMIN_MENUS_UPDATE_FORM_PREPARE = 'sfs_cms.admin.menus.update_form_prepare';
+    public const ADMIN_MENUS_UPDATE_FORM_INIT = 'sfs_cms.admin.menus.update_form_init';
+    public const ADMIN_MENUS_UPDATE_FORM_VALID = 'sfs_cms.admin.menus.update_form_valid';
+    public const ADMIN_MENUS_UPDATE_SUCCESS = 'sfs_cms.admin.menus.update_success';
+    public const ADMIN_MENUS_UPDATE_FAILURE = 'sfs_cms.admin.menus.update_failure';
+    public const ADMIN_MENUS_UPDATE_FORM_INVALID = 'sfs_cms.admin.menus.update_form_invalid';
+    public const ADMIN_MENUS_UPDATE_VIEW = 'sfs_cms.admin.menus.update_view';
+
     public const ADMIN_ROUTES_LIST_INITIALIZE = 'sfs_cms.admin.routes.list_initialize';
     public const ADMIN_ROUTES_LIST_VIEW = 'sfs_cms.admin.routes.list_view';
     public const ADMIN_ROUTES_CREATE_INITIALIZE = 'sfs_cms.admin.routes.create_initialize';
