4034d9bb886efa11536c2a350f0587cef01f0cba
Javi H. Gil <javihgil@softspring.es>
2024-02-02 14:33:28 +0100

    Fix code style

--- src/Sitemap/InvalidSitemapException.php
+++ src/Sitemap/InvalidSitemapException.php
@@ -4,7 +4,7 @@ namespace Softspring\CmsBundle\Sitemap;
 
 class InvalidSitemapException extends \Exception
 {
-    public function __construct(string $sitemap, int $code = 0, ?\Throwable $previous = null)
+    public function __construct(string $sitemap, int $code = 0, \Throwable $previous = null)
     {
         parent::__construct(sprintf('Invalid "%s" sitemap for this site', $sitemap), $code, $previous);
     }
