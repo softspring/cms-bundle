948cf313b79bd2afa534f43fd5eeb05161d92d0d
Javi H. Gil <javihgil@softspring.es>
2024-02-19 15:27:19 +0100

    Refactor SEO data to versions

--- src/Form/Admin/ContentVersion/VersionSeoForm.php
+++ src/Form/Admin/ContentVersion/VersionSeoForm.php
@@ -1,26 +1,35 @@
 <?php
 
-namespace Softspring\CmsBundle\Form\Admin\Content;
+namespace Softspring\CmsBundle\Form\Admin\ContentVersion;
 
 use Softspring\CmsBundle\Form\Type\DynamicFormType;
 use Softspring\CmsBundle\Model\ContentInterface;
+use Softspring\CmsBundle\Model\ContentVersionInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class ContentSeoForm extends AbstractType
+class VersionSeoForm extends AbstractType
 {
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => ContentInterface::class,
+            'data_class' => ContentVersionInterface::class,
             'label_format' => 'admin_content.form.%name%.label',
             'validation_groups' => ['Default', 'create', 'update'],
             'translation_domain' => 'sfs_cms_contents',
+            'content_type' => null,
+            'content_config' => null,
         ]);
 
         $resolver->setRequired('content_config');
+
+        $resolver->setRequired('content_type');
+        $resolver->setAllowedTypes('content_type', ['string']);
+
+        $resolver->setRequired('content');
+        $resolver->setAllowedTypes('content', [ContentInterface::class]);
 
         $resolver->setNormalizer('label_format', function (Options $options, $value) {
             return "admin_{$options['content_config']['_id']}.form.%name%.label";
@@ -30,7 +39,7 @@ class ContentSeoForm extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder->add('seo', DynamicFormType::class, [
-            'form_fields' => $options['content_config']['seo'] ?? [],
+            'form_fields' => $options['content_config']['version_seo'] ?? [],
             'translation_domain' => 'sfs_cms_contents',
             'label_format' => "admin_{$options['content_config']['_id']}.form.seo.%name%.label",
         ]);
