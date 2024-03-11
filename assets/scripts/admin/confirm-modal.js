fca16b7db2172fb0cfebe63755bba0e54679d9a9
Javi H. Gil <javihgil@users.noreply.github.com>
2024-03-11 13:21:11 +0100

    Confirm bootstrap modal (#258)

---
+++ assets/scripts/admin/confirm-modal.js
@@ -0,0 +1,79 @@
+window.addEventListener('load', _init);
+
+function decodeMessage(encoded) {
+    return decodeURIComponent(atob(encoded).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
+}
+
+function _init() {
+    document.addEventListener('click', function (event) {
+        if (!event.target.matches('[data-confirm-modal]')) return;
+
+        event.preventDefault();
+
+        const title = event.target.dataset.confirmModalTitle || '';
+        const message = decodeMessage(event.target.dataset.confirmModal || '');
+        const confirmButton = event.target.dataset.confirmModalConfirmButton || 'Continue';
+        const confirmButtonType = event.target.dataset.confirmModalConfirmButtonType || 'primary';
+        const cancelButton = event.target.dataset.confirmModalCancelButton || 'Cancel';
+
+        if (!event.target.href) {
+            console.error('Only links are supported for confirm modal');
+            return;
+        }
+
+        confirmModalLink(title, message, confirmButton, confirmButtonType, cancelButton, event.target.href);
+
+        return false;
+    });
+}
+
+function confirmModalLink(title, message, confirmButton, confirmButtonType, cancelButton, url) {
+    const modalId = createModal(title, message, confirmButton, confirmButtonType, cancelButton, url);
+    const modalDiv = document.getElementById(modalId);
+    const modal = new bootstrap.Modal(modalDiv, {
+        keyboard: false
+    });
+    modal.show();
+}
+
+function createModal(title, message, confirmButton, confirmButtonType, cancelButton, url=null) {
+    const modalRandomId = Math.random().toString(36).substring(7);
+
+    let modalHeader = '';
+    if (title) {
+        modalHeader = `<div class="modal-header">
+        <h5 class="modal-title">${title}</h5>
+        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>`;
+    }
+
+    let confirmButtonElement = '';
+    if (url) {
+        confirmButtonElement = `<a href="${url}" class="btn btn-${confirmButtonType}">${confirmButton}</a>`;
+    } else {
+        confirmButtonElement = `<button type="button" class="btn btn-${confirmButtonType}">${confirmButton}</button>`;
+    }
+
+    const modalHtml = `
+    <div class="modal" tabindex="-1" id="${modalRandomId}">
+  <div class="modal-dialog modal-dialog-centered">
+    <div class="modal-content">
+      ${modalHeader}  
+      <div class="modal-body">${message}</div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">${cancelButton}</button>
+        ${confirmButtonElement}
+      </div>
+    </div>
+  </div>
+</div>`;
+
+    const modalNode = document.createElement('div');
+    document.body.appendChild(modalNode);
+    modalNode.outerHTML = modalHtml;
+
+    return modalRandomId;
+}
+
