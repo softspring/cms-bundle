b9615ffdf1a7d6fe380bbe30b670c5174983c416
Javi H. Gil <javihgil@softspring.es>
2024-04-09 12:08:44 +0200

    Build assets

---
+++ assets/build-admin.js
@@ -0,0 +1,16 @@
+// any CSS you import will output into a single css file (app.css in this case)
+import './build-admin.scss';
+
+// import bootstrap 5 js
+import * as bootstrap from 'bootstrap';
+window.bootstrap = bootstrap;
+// import 'tinymce';
+// import 'tinymce/models/dom/model';
+// import 'tinymce/icons/default';
+// import 'tinymce/themes/silver';
+
+import './scripts/admin-cms';
+// import '@softspring/media-bundle/scripts/media-type';
+// import '@softspring/polymorphic-form-type/scripts/polymorphic-form-type';
+// import '@softspring/cms-module-collection/modules';
+
