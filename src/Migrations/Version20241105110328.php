e535c9bc45dbadb07817db3048cd95435d13ee32
Javi H. Gil <javihgil@users.noreply.github.com>
2024-11-14 17:45:59 +0100

    Extract translation form type to new bundle (#330)
    * Extract translation form type to new bundle
    
    * Fix TranslatableType
    
    * Add route id max length to support actual doctrine versions
    
    * Some updates

---
+++ src/Migrations/Version20241105110328.php
@@ -0,0 +1,52 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Softspring\CmsBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20241105110328 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Set up route ids to be 100 chars long';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE cms_content_version_routes DROP FOREIGN KEY FK_62372FCD34ECB4E6');
+        $this->addSql('ALTER TABLE cms_route_path DROP FOREIGN KEY FK_95D1F10F34ECB4E6');
+        $this->addSql('ALTER TABLE cms_route DROP FOREIGN KEY FK_2CB7BB55727ACA70');
+        $this->addSql('ALTER TABLE cms_route_sites DROP FOREIGN KEY FK_BC9E2F1934ECB4E6');
+
+        $this->addSql('ALTER TABLE cms_content_version_routes CHANGE route_id route_id VARCHAR(100) NOT NULL');
+        $this->addSql('ALTER TABLE cms_route CHANGE id id VARCHAR(100) NOT NULL, CHANGE parent_id parent_id VARCHAR(100) DEFAULT NULL');
+        $this->addSql('ALTER TABLE cms_route_sites CHANGE route_id route_id VARCHAR(100) NOT NULL');
+        $this->addSql('ALTER TABLE cms_route_path CHANGE route_id route_id VARCHAR(100) DEFAULT NULL');
+
+        $this->addSql('ALTER TABLE cms_content_version_routes ADD CONSTRAINT FK_62372FCD34ECB4E6 FOREIGN KEY (route_id) REFERENCES cms_route (id) ON DELETE RESTRICT');
+        $this->addSql('ALTER TABLE cms_route_path ADD CONSTRAINT FK_95D1F10F34ECB4E6 FOREIGN KEY (route_id) REFERENCES cms_route (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE cms_route_sites ADD CONSTRAINT FK_BC9E2F1934ECB4E6 FOREIGN KEY (route_id) REFERENCES cms_route (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE cms_route ADD CONSTRAINT FK_2CB7BB55727ACA70 FOREIGN KEY (parent_id) REFERENCES cms_route (id) ON DELETE RESTRICT');
+    }
+
+    public function down(Schema $schema): void
+    {
+        $this->addSql('ALTER TABLE cms_content_version_routes DROP FOREIGN KEY FK_62372FCD34ECB4E6');
+        $this->addSql('ALTER TABLE cms_route_path DROP FOREIGN KEY FK_95D1F10F34ECB4E6');
+        $this->addSql('ALTER TABLE cms_route DROP FOREIGN KEY FK_2CB7BB55727ACA70');
+        $this->addSql('ALTER TABLE cms_route_sites DROP FOREIGN KEY FK_BC9E2F1934ECB4E6');
+
+        $this->addSql('ALTER TABLE cms_content_version_routes CHANGE route_id route_id VARCHAR(13) NOT NULL');
+        $this->addSql('ALTER TABLE cms_route CHANGE id id VARCHAR(13) NOT NULL, CHANGE parent_id parent_id VARCHAR(13) DEFAULT NULL');
+        $this->addSql('ALTER TABLE cms_route_path CHANGE route_id route_id VARCHAR(13) DEFAULT NULL');
+        $this->addSql('ALTER TABLE cms_route_sites CHANGE route_id route_id VARCHAR(13) NOT NULL');
+
+        $this->addSql('ALTER TABLE cms_content_version_routes ADD CONSTRAINT FK_62372FCD34ECB4E6 FOREIGN KEY (route_id) REFERENCES cms_route (id) ON DELETE RESTRICT');
+        $this->addSql('ALTER TABLE cms_route_path ADD CONSTRAINT FK_95D1F10F34ECB4E6 FOREIGN KEY (route_id) REFERENCES cms_route (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE cms_route_sites ADD CONSTRAINT FK_BC9E2F1934ECB4E6 FOREIGN KEY (route_id) REFERENCES cms_route (id) ON DELETE CASCADE');
+        $this->addSql('ALTER TABLE cms_route ADD CONSTRAINT FK_2CB7BB55727ACA70 FOREIGN KEY (parent_id) REFERENCES cms_route (id) ON DELETE RESTRICT');
+    }
+}
