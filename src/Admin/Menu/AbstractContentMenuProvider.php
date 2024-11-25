f43bb4bd8a7cd26b49fe51f90bbc2693a799ca53
Javi H. Gil <javihgil@softspring.es>
2024-11-25 11:27:52 +0100

    Remove deprecated toString uses in route generation

--- src/Admin/Menu/AbstractContentMenuProvider.php
+++ src/Admin/Menu/AbstractContentMenuProvider.php
@@ -25,7 +25,7 @@ abstract class AbstractContentMenuProvider implements ContentMenuProviderInterfa
     protected function getMenuItem(string $id, string $current, ContentInterface $content, string $contentType, array $contentConfig, ?string $configKey = null): MenuItem
     {
         $text = $this->translator->trans("admin_{$contentType}.tabs_menu.$id", [], 'sfs_cms_contents');
-        $url = $this->router->generate("sfs_cms_admin_content_{$contentType}_{$id}", ['content' => $content]);
+        $url = $this->router->generate("sfs_cms_admin_content_{$contentType}_{$id}", ['content' => $content->getId()]);
         $active = $current == $id;
         $isGranted = $contentConfig['admin'][$configKey ?? $id]['is_granted'] ?? null;
         $disabled = ('#' === $url) || ($isGranted && !$this->authorizationChecker->isGranted($isGranted, $content));
