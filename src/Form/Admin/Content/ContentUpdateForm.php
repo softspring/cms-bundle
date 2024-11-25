a46aa4f1cb46659b80b0615f8b8ac33b3e1df253
Javi H. Gil <javihgil@users.noreply.github.com>
2024-11-25 12:18:03 +0100

    Content update form allows to add and remove sites and locales (#342)

--- src/Form/Admin/Content/ContentUpdateForm.php
+++ src/Form/Admin/Content/ContentUpdateForm.php
@@ -2,6 +2,7 @@
 
 namespace Softspring\CmsBundle\Form\Admin\Content;
 
+use Softspring\CmsBundle\Form\Admin\SiteChoiceType;
 use Softspring\CmsBundle\Form\Type\DynamicFormType;
 use Softspring\CmsBundle\Model\ContentInterface;
 use Softspring\CmsBundle\Translator\TranslatableContext;
@@ -12,6 +13,7 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Intl\Locales;
 use Symfony\Component\OptionsResolver\Options;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Count;
 
 class ContentUpdateForm extends AbstractType implements ContentUpdateFormInterface
 {
@@ -33,6 +35,10 @@ class ContentUpdateForm extends AbstractType implements ContentUpdateFormInterfa
             return "admin_{$options['content_config']['_id']}.form.%name%.label";
         });
 
+        $resolver->setDefault('default_locale', $this->translatableContext->getDefaultLocale());
+        $resolver->setRequired('default_locale');
+        $resolver->setAllowedTypes('default_locale', ['string']);
+
         $resolver->setDefault('locales', $this->translatableContext->getEnabledLocales());
         $resolver->setRequired('locales');
         $resolver->setAllowedTypes('locales', ['array']);
@@ -45,17 +51,26 @@ class ContentUpdateForm extends AbstractType implements ContentUpdateFormInterfa
     {
         $builder->add('name', TextType::class);
 
-        $content = $options['content'];
-        $addLocales = array_diff($options['locales'], $content->getLocales());
-        if (!empty($addLocales)) {
-            $builder->add('addLocale', ChoiceType::class, [
-                'expanded' => true,
-                'multiple' => true,
-                'choice_translation_domain' => false,
-                'choices' => array_combine(array_map(fn ($lang) => Locales::getName($lang), $addLocales), $addLocales),
-                'mapped' => false,
-            ]);
-        }
+        $builder->add('defaultLocale', ChoiceType::class, [
+            'choice_translation_domain' => false,
+            'choices' => array_combine(array_map(fn ($lang) => Locales::getName($lang), $options['locales']), $options['locales']),
+            'default_value' => $options['default_locale'],
+        ]);
+
+        $builder->add('locales', ChoiceType::class, [
+            'multiple' => true,
+            'expanded' => true,
+            'choice_translation_domain' => false,
+            'choices' => array_combine(array_map(fn ($lang) => Locales::getName($lang), $options['locales']), $options['locales']),
+            'constraints' => new Count(['min' => 1]),
+            'default_value' => [$options['default_locale']],
+        ]);
+
+        $builder->add('sites', SiteChoiceType::class, [
+            'content' => $options['content_config'],
+            'by_reference' => false,
+            'constraints' => new Count(['min' => 1]),
+        ]);
 
         if (!empty($options['content_config']['extra_fields'])) {
             $builder->add('extraData', DynamicFormType::class, [
