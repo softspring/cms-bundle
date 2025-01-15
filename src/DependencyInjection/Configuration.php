47cda4b0e796cadc4b4c766b6fd4517b222e5bce
Javi H. Gil <javihgil@softspring.es>
2025-01-15 09:02:21 +0100

    Rollback configuration change, it's needed for config merging

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -42,8 +42,9 @@ class Configuration implements ConfigurationInterface
                         unset($configuration['content']['cache_last_modified']);
                     }
 
+                    /** @phpstan-ignore-next-line */
                     $menuCacheEnabled = $configuration['menu']['cache']['enabled'] ?? $defaultCacheEnabled ?? null;
-                    if (isset($menuCacheEnabled)) {
+                    if ($menuCacheEnabled) {
                         $configuration['menu']['cache']['enabled'] = $menuCacheEnabled;
                         $configuration['menu']['cache']['type'] = $configuration['menu']['cache']['type'] ?? $defaultCacheType;
                         if ('ttl' !== $configuration['menu']['cache']['type']) {
@@ -51,8 +52,9 @@ class Configuration implements ConfigurationInterface
                         }
                     }
 
+                    /** @phpstan-ignore-next-line */
                     $blockCacheEnabled = $configuration['block']['cache']['enabled'] ?? $defaultCacheEnabled ?? null;
-                    if (isset($blockCacheEnabled)) {
+                    if ($blockCacheEnabled) {
                         $configuration['block']['cache']['enabled'] = $blockCacheEnabled;
                         $configuration['block']['cache']['type'] = $configuration['block']['cache']['type'] ?? $defaultCacheType;
                         if ('ttl' !== $configuration['block']['cache']['type']) {
