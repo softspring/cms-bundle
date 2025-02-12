65254cdff041d876410527c133c7748bc600a851
Javi H. Gil <javihgil@softspring.es>
2025-02-12 15:15:31 +0100

    Do not save compiled if disabled

--- src/Manager/ContentVersionManager.php
+++ src/Manager/ContentVersionManager.php
@@ -72,7 +72,7 @@ class ContentVersionManager implements ContentVersionManagerInterface
             'key' => $compiledKey,
         ]);
 
-        if (!$compiledData?->getDataPart('content')) {
+        if (!$compiledData?->getDataPart('content') || !$this->contentCompiler->canSaveCompiled($contentVersion)) {
             $compiledData = $this->contentCompiler->compileRequest($contentVersion, $request, $compiledData?->getDataPart('modules'), $throwExceptionOnCompileError);
             $this->contentCompiler->canSaveCompiled($contentVersion) && $this->saveEntity($contentVersion);
         }
