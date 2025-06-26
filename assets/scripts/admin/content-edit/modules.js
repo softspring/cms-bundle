25b074384510187d82bfa0f9150ef61223ab6a8e
Javi H. Gil <javihgil@softspring.es>
2025-06-26 13:00:08 +0200

    Integrate collection-form-type copy&paste feature

--- assets/scripts/admin/content-edit/modules.js
+++ assets/scripts/admin/content-edit/modules.js
@@ -1,7 +1,9 @@
+// let collectionFormTypeDebug = true;
+
 import {filterCurrentFilterElements} from './filter-preview';
-import {getCollectionLastIndex} from '@softspring/collection-form-type/scripts/collection-form-type';
+import {getCollectionLastIndex, CollectionEvent} from '@softspring/collection-form-type/scripts/collection-form-type';
 import {Modal} from 'bootstrap';
-import {registerFeature} from '@softspring/cms-bundle/scripts/tools';
+import {registerFeature, showAlert} from '@softspring/cms-bundle/scripts/tools';
 
 registerFeature('admin_content_edit_modules', _init);
 
@@ -53,6 +55,34 @@ function _init() {
     });
 
     document.addEventListener("collection.node.duplicate.after", function (event) { // (1)
+        var module = event.target.querySelector('.cms-module');
+        if (module) {
+            moduleFocus(module);
+        }
+        filterCurrentFilterElements();
+    });
+
+    document.addEventListener("collection.node.copy.after", function (event) { // (1)
+        showAlert('Copied to clipboard!', 'secondary', 800);
+    });
+
+    document.addEventListener("collection.node.copy.prepare", function (event) { // (1)
+        let data = event.toArray();
+        data.source = 'softspring/cms-bundle';
+        // add md5 hash of the data
+        event.data(data);
+    });
+
+    document.addEventListener("collection.node.paste.validate", function (event) { // (1)
+        let data = event.toArray();
+
+        if (!data || !data.source || data.source !== 'softspring/cms-bundle') {
+            event.data(null);
+        } else {
+            event.data(data);
+        }
+    });
+    document.addEventListener("collection.node.paste.after", function (event) { // (1)
         var module = event.target.querySelector('.cms-module');
         if (module) {
             moduleFocus(module);
@@ -146,6 +176,42 @@ function _init() {
     /**
      * @param {CollectionEvent} event
      */
+    document.addEventListener("collection.node.paste.before", function (event) {
+        if (modulesCollection) {
+            event.collection(modulesCollection);
+            event.position(modulesCollectionInsertIndex !== null ? modulesCollectionInsertIndex : getCollectionLastIndex(modulesCollection) + 1);
+        } else if (event.target && event.target.dataset.collectionTarget !== undefined) {
+            modulesCollection = document.getElementById(event.target.dataset.collectionTarget);
+        } else {
+            throw new Error('No collection target found for module');
+        }
+
+        // const moduleThumbnail = event._originEvent.target;
+        // let prototype = moduleThumbnail.dataset.prototype ?? moduleThumbnail.dataset.collectionPrototype;
+        // // COLLECTION
+        // // version_create_form_data_main_1_modules_0
+        // // version_create_form[data][main][1][modules][0]
+        // // PROTOTYPE
+        // // version_create_form_module_prototypes_collection____MODULE____class
+        // // version_create_form[module_prototypes_collection][___MODULE___]
+        // // RESULT
+        // // version_create_form_data_main_1_modules_0_modules_0_class
+        // // version_create_form[data][main][1][modules][0][modules][0][class]
+        // prototype = prototype.replace(new RegExp('version_create_form_module_prototypes_collection', 'g'), modulesCollection.id);
+        // prototype = prototype.replace(new RegExp('version_create_form\\[module_prototypes_collection\\]', 'g'), modulesCollection.dataset.fullName);
+        // event.prototype(prototype);
+
+        // reset variables
+        modulesCollection = null;
+        modulesCollectionInsertIndex = null;
+
+        const modal = Modal.getInstance(prototypesModal);
+        modal && modal.hide();
+    });
+
+    /**
+     * @param {CollectionEvent} event
+     */
     document.addEventListener("collection.node.insert.after", function (event) {
         if (!event.collection() || !event.node() || event.collection().dataset.moduleRowClass === undefined) {
             return;
@@ -154,7 +220,7 @@ function _init() {
         event.node().classList.remove(...event.node().classList);
         event.node().classList.add(event.collection().dataset.moduleRowClass);
 
-        if (event.collection().dataset.moduleRowClass == 'col') {
+        if (event.collection().dataset.moduleRowClass === 'col') {
             const down = event.node().querySelector(':scope > .cms-module > .cms-module-header > .cms-module-buttons > [data-collection-action=down] .bi-chevron-down');
             down.classList.remove('bi-chevron-down');
             down.classList.add('bi-chevron-right');
@@ -179,6 +245,7 @@ function _init() {
     document.addEventListener("collection.node.insert.after", checkMaxInputVars);
     document.addEventListener("collection.node.add.after", checkMaxInputVars);
     document.addEventListener("collection.node.duplicate.after", checkMaxInputVars);
+    document.addEventListener("collection.node.paste.after", checkMaxInputVars);
 }
 
 function moduleFocus(module) {
