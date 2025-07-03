c791c19e74309bf4ae9d0de84c2d9ce48c7d3522
ebpe <ebelmonte82@gmail.com>
2025-07-03 12:18:48 +0200

    Fix duplicated backdrop when open add module modal

--- assets/scripts/admin/content-edit/modules.js
+++ assets/scripts/admin/content-edit/modules.js
@@ -12,6 +12,9 @@ registerFeature('admin_content_edit_modules', _init);
  * @private
  */
 function _init() {
+
+    checkEmptyModules();
+
     // close module edit form
     document.addEventListener('click', function (event) {
         if (!event.target || !event.target.hasAttribute('data-cms-module-form-close')) return;
@@ -44,6 +47,7 @@ function _init() {
             moduleFocus(module);
         }
         filterCurrentFilterElements();
+        checkEmptyModules();
     });
 
     document.addEventListener("collection.node.add.after", function (event) { // (1)
@@ -52,6 +56,7 @@ function _init() {
             moduleFocus(module);
         }
         filterCurrentFilterElements();
+        checkEmptyModules();
     });
 
     document.addEventListener("collection.node.duplicate.after", function (event) { // (1)
@@ -60,6 +65,7 @@ function _init() {
             moduleFocus(module);
         }
         filterCurrentFilterElements();
+        checkEmptyModules();
     });
 
     document.addEventListener("collection.node.copy.after", function () { // (1)
@@ -127,6 +133,7 @@ function _init() {
             moduleFocus(module);
         }
         filterCurrentFilterElements();
+        checkEmptyModules();
     });
 
     const prototypesModal = document.getElementById('module_prototypes_collection_modal');
@@ -321,5 +328,18 @@ function checkMaxInputVars() {
     } else {
         [...buttons].forEach((button) => button.classList.remove('disabled'));
         maxInputVarsMessage.classList.add('d-none');
+    }
+}
+
+function checkEmptyModules() {
+    // check if we have a module form
+    const modules = document.querySelectorAll('[data-collection="collection"] .insert-module');
+
+    if (modules.length === 1) {
+        modules[0].classList.add('active');
+        modules[0].closest('[data-collection="collection"]').classList.add('active');
+    } else {
+        modules[0].classList.remove('active');
+        modules[0].closest('[data-collection="collection"]').classList.remove('active');
     }
 }
