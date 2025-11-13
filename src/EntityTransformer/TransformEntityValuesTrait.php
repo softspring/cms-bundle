3cb175f3002bacec8938c471919745ff1e8c5385
Javi H. Gil <javihgil@users.noreply.github.com>
2025-11-13 13:16:09 +0100

    Untransform translations values to prevent non transformed fields (#442)

--- src/EntityTransformer/TransformEntityValuesTrait.php
+++ src/EntityTransformer/TransformEntityValuesTrait.php
@@ -54,7 +54,11 @@ trait TransformEntityValuesTrait
                 return $this->_references[$value['_entity_class']][$serializedId];
             } elseif (isset($value['_trans_id']) && isset($value['_default']) && (isset($value[$value['_default']]) && is_string($value[$value['_default']]) || is_null($value[$value['_default']] ?? null))) {
                 // if we are sure that this is a translation, we can create a new Translation object
-                return Translation::createFromArray($value);
+                $value = Translation::createFromArray($value);
+                // iterate over translation values to untransform possible entities inside
+                foreach ($value as $key => $value2) {
+                    $value[$key] = $this->untransformEntityValues($value2, $objectManager);
+                }
             } else {
                 foreach ($value as $key => $value2) {
                     $value[$key] = $this->untransformEntityValues($value2, $objectManager);
